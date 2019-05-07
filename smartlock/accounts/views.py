ee99595be92f766d287046d15080c37aa1b432e2
nami-kt <43203057+nami-kt@users.noreply.github.com>
2019-05-07 20:04:29 +0200

    Update views.py

--- smartlock/accounts/views.py
+++ smartlock/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, authenticate
-from smartlock.forms import SignUpForm
+from django.contrib.auth import authenticate
+from accounts.forms.forms import SignUpForm
 
 
 def signup(request):
@@ -12,17 +12,12 @@ def signup(request):
             username = form.cleaned_data.get('username')
             first_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=first_password)
-            login(request, user)
+            user.save()
+            #login(request, user)
             return redirect('home')
         else:
             form = UserCreationForm()
             return render(request, 'signup.html', {'form': form})
     else:
-        form = UserCreationForm()
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
-
-
-
-        
-            
