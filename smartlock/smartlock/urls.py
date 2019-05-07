b4807b2380985bb4a28574af19f2290ce79bfebd
nami-kt <43203057+nami-kt@users.noreply.github.com>
2019-05-07 20:03:46 +0200

    Update urls.py

--- smartlock/smartlock/urls.py
+++ smartlock/smartlock/urls.py
@@ -1,32 +1,17 @@
-"""smartlock URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
-from smartlock.forms import AuthForm, SignUpForm
+from accounts.forms.forms import AuthForm, SignUpForm
+from django.contrib.auth.views import LoginView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')), #accounts app, must be above the auth app
     url(r'^accounts/', include('django.contrib.auth.urls')), #include the auth app
-    url(r'^login/$', auth_views.login, name='login', kwargs={"authentication_form": AuthForm}),
+    url(r'^login/$', LoginView.as_view(template_name='registration/login.html'), name='login', kwargs={"authentication_form": AuthForm}),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'), #home page
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
