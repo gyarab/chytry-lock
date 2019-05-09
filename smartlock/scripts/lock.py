5074d7ae8c4be6da4b9a8b6e0b1e1dea3db9d10d
antoninjirasek <44426074+antoninjirasek@users.noreply.github.com>
2019-05-09 16:38:14 +0200

    Update lock.py

--- smartlock/scripts/lock.py
+++ smartlock/scripts/lock.py
@@ -2,8 +2,10 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(3, GPIO.OUT)
+GPIO.setup(37, GPIO.OUT)
+GPIO.setup(7, GPIO.OUT)
 
-GPIO.output(3, True)
+GPIO.output(37, False)
+GPIO.output(7, True)
 time.sleep(2)	
 GPIO.cleanup()
