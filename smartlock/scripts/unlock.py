ad986d98eec7b0ae08055cfbd0abcc9be0d7c3e2
antoninjirasek <44426074+antoninjirasek@users.noreply.github.com>
2019-05-09 16:39:08 +0200

    Update unlock.py

--- smartlock/scripts/unlock.py
+++ smartlock/scripts/unlock.py
@@ -3,8 +3,10 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(37, GPIO.OUT)
+GPIO.setup(7, GPIO.OUT)
 
+GPIO.output(7, False)
 GPIO.output(37, True)
-time.sleep(0.5)	
+time.sleep(2)	
 GPIO.cleanup()
 
